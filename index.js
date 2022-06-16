01e278cf72063323914ab0368283bb396db2870e
richard gichuki <chadgichuki@gmail.com>
2022-06-16 15:40:31 +0300

    function works correctly

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Outer loop through the array
+  for (let i=0; i<array.length; i++){
+    // Inner loop through the rest of the array
+    for (let j=0; j<array.length; j++){
+      // The number should skip itself
+      if (i !== j){
+        let sum = array[i] + array[j]
+        // check if this sum is equal to target
+        if (sum === target) return true
+      }
+    }
+  }
+  // Return false if no comparison evaluates to true
+  return false
 }
 
 /* 
@@ -8,6 +21,15 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+
+  Loop through the array one number at a time
+  For each number:
+    Loop through the rest of the array.(Somehow skip the current number so you don't add it by itself)
+    check if the sum of your current number + the other number is === to yor target
+      return true if total matches
+    else
+      return false
+
 */
 
 /*
@@ -29,6 +51,17 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1, 2, 3], 7));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([0, 0, 0], 0));
 }
+
 
 module.exports = hasTargetSum;
